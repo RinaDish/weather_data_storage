159064f70643dea5adf8761b66bd5572126d7033
Rina Dishli <dishli271113@gmail.com>
2021-03-09 20:22:26 +0200

    feat: added full list of cities

---
+++ src/citiesList.js
@@ -0,0 +1,21 @@
+const citiesList = [
+  'Amsterdam', 'Athens', 'Belgrade',
+  'Berlin', 'Bern', 'Bratislava',
+  'Brussels', 'Bucharest', 'Budapest',
+  'Chisinau', 'Copenhagen', 'Douglas',
+  'Dublin', 'Gibraltar', 'Helsinki',
+  'Kiev', 'Lisbon', 'Ljubljana',
+  'London', 'Luxembourg', 'Madrid',
+  'Minsk', 'Monaco', 'Moscow',
+  'Nicosia', 'North Nicosia', 'Oslo',
+  'Paris', 'Podgorica', 'Prague',
+  'Pristina', 'Reykjavik', 'Riga',
+  'Rome', 'San Marino', 'Sarajevo',
+  'Skopje', 'Sofia', 'Stockholm',
+  'Tallinn', 'Tbilisi', 'Tirana',
+  'Vaduz', 'Valletta', 'Vatican City',
+  'Vienna', 'Vilnius', 'Warsaw',
+  'Yerevan', 'Zagreb',
+];
+
+module.exports = citiesList;

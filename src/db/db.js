b2d64e7a6831d12a96f7365fcf60f9dd05c32f45
Rina Dishli <dishli271113@gmail.com>
2021-03-10 17:01:13 +0200

    docs: moved db options to config.env file

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  password: 'pigeon',
-  host: 'localhost',
-  port: '5432',
-  database: 'weather_storage',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
